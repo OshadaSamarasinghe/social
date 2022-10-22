2d5292d5a87090a7a34192f5c57dd949943b15c9
Oshada Samarasinghe <oshadasa@gmail.com>
2022-10-23 01:32:32 +0530

    Encrypt Password

--- Social_API/routes/auth.js
+++ Social_API/routes/auth.js
@@ -1,13 +1,17 @@
 const router = require("express").Router();
-const User = require("../models/User")
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 router.post("/register", async(req,res)=>{
-    const newUser = new User({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password,
-    });
+    
     try{
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPassword,
+        })
         const user = await newUser.save();
         res.status(200).json(user);
     }   catch(err){
