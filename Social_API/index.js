634534617401c16d7db66f86194b4fba609b3c99
Oshada Samarasinghe <oshadasa@gmail.com>
2022-10-22 19:05:21 +0530

    created routes

--- Social_API/index.js
+++ Social_API/index.js
@@ -1,2 +1,25 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+
+dotenv.config();
+
+mongoose.connect(process.env.mongodb_url,()=>{
+    console.log("Connected to MongoDB")
+});
+
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+
+app.listen(8080,()=>{
+    console.log("Backend server is running")
+})
