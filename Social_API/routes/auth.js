0fd78a3eaf1d681f77cc73c7d8718d042cf27e48
Oshada Samarasinghe <oshadasa@gmail.com>
2022-10-23 00:55:51 +0530

    Creating modues

--- Social_API/routes/auth.js
+++ Social_API/routes/auth.js
@@ -1,5 +1,17 @@
 const router = require("express").Router();
-router.get("/",(req,res)=>{
-    res.send("Auth Route")
+const User = require("../models/User")
+
+router.post("/register", async(req,res)=>{
+    const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+    });
+    try{
+        const user = await newUser.save();
+        res.status(200).json(user);
+    }   catch(err){
+            console.log(err)
+    }
 })
 module.exports = router;
