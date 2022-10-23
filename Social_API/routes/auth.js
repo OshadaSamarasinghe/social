8de63b425929d58b7c68059456fc9d302caa93bc
Oshada Samarasinghe <oshadasa@gmail.com>
2022-10-24 02:02:58 +0530

    Creating login and editing "User" model

--- Social_API/routes/auth.js
+++ Social_API/routes/auth.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
+const { json } = require("express");
 
+// Register
 router.post("/register", async(req,res)=>{
-    
     try{
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(req.body.password, salt);
@@ -14,8 +15,23 @@ router.post("/register", async(req,res)=>{
         })
         const user = await newUser.save();
         res.status(200).json(user);
-    }   catch(err){
-            console.log(err)
+    }catch(err){
+        res.status(500).json(err)
     }
-})
+});
+// Login
+router.post("/login", async(req,res)=>{
+    try{
+        // check email
+        const user = await User.findOne({email:req.body.email});
+        !user && res.status(404).json("User not found");
+        // check password
+        const validPass = await bcrypt.compare(req.body.password, user.password);
+        !validPass && res.status(400).json("Invalid Password");
+        // if enter valid email and password
+        res.status(200).json(user)
+    }catch(err){
+        res.status(500).json(err)
+    }
+});
 module.exports = router;
--- Social_API/routes/users.js
+++ Social_API/routes/users.js
@@ -2,4 +2,6 @@ const router = require("express").Router();
 router.get("/",(req,res)=>{
     res.send("User Route")
 })
+
+
 module.exports = router;
