3b9ea548fc221c41d51c0c4a6558d5c3346d816f
Oshada Samarasinghe <oshadasa@gmail.com>
2022-10-24 16:26:04 +0530

    Update User data

--- Social_API/routes/auth.js
+++ Social_API/routes/auth.js
@@ -12,7 +12,7 @@ router.post("/register", async(req,res)=>{
             username: req.body.username,
             email: req.body.email,
             password: hashedPassword,
-        })
+        });
         const user = await newUser.save();
         res.status(200).json(user);
     }catch(err){
@@ -29,9 +29,9 @@ router.post("/login", async(req,res)=>{
         const validPass = await bcrypt.compare(req.body.password, user.password);
         !validPass && res.status(400).json("Invalid Password");
         // if enter valid email and password
-        res.status(200).json(user)
+        res.status(200).json(user);
     }catch(err){
-        res.status(500).json(err)
+        res.status(500).json(err);
     }
 });
 module.exports = router;
--- Social_API/routes/users.js
+++ Social_API/routes/users.js
@@ -1,7 +1,30 @@
+const { json } = require("express");
+const User = require("../models/User");
 const router = require("express").Router();
-router.get("/",(req,res)=>{
-    res.send("User Route")
-})
+const bcrypt = require("bcrypt");
 
+// Update users
+router.put("/:id", async(req,res)=>{
+    if (req.body.userId === req.params.id || req.user.isAdmin) {
+        if (req.body.password) {
+            try {
+                const salt = await bcrypt.genSalt(10);
+                req.body.password = await bcrypt.hash(req.body.password, salt);
+            } catch (err) {
+                return res.status(500).json(err);
+            }
+        }
+        try {
+            const user = await User.findByIdAndUpdate(req.params.id, {
+                $set: req.body,
+            });
+            res.status(200).json("Account has been Updated")
+        } catch (err) {
+            return res.status(500).json(err);
+        }
+    }else{
+        return res.status(403),json("you can update only your account");
+    }
+});
 
 module.exports = router;
