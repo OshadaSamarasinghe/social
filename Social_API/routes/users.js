16445885f8d54fd027d989d4a1c3606a7e391db2
Oshada Samarasinghe <oshadasa@gmail.com>
2022-10-25 16:51:55 +0530

    Deleting users

--- Social_API/routes/users.js
+++ Social_API/routes/users.js
@@ -5,7 +5,7 @@ const bcrypt = require("bcrypt");
 
 // Update users
 router.put("/:id", async(req,res)=>{
-    if (req.body.userId === req.params.id || req.user.isAdmin) {
+    if (req.body.userId === req.params.id || req.body.isAdmin) {
         if (req.body.password) {
             try {
                 const salt = await bcrypt.genSalt(10);
@@ -22,8 +22,21 @@ router.put("/:id", async(req,res)=>{
         } catch (err) {
             return res.status(500).json(err);
         }
-    }else{
-        return res.status(403),json("you can update only your account");
+    }   else {
+        return res.status(403).json("you can update only your account");
+    }
+});
+// delete users
+router.delete("/:id", async(req,res)=>{
+    if (req.body.userId === req.params.id || req.body.isAdmin) {
+        try {
+            await User.findByIdAndDelete(req.params.id);
+            res.status(200).json("Account has been Deleted");
+        } catch (err) {
+            return res.status(500).json(err);
+        }
+    }   else {
+        return res.status(403).json("you can delete only your account");
     }
 });
 
