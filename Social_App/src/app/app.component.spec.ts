e91f983f0ed29bdc3fa892642127108c50708056
Oshada Samarasinghe <oshadasa@gmail.com>
2022-11-01 17:59:42 +0530

    Creating app UI

--- Social_App/src/app/app.component.spec.ts
+++ Social_App/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Social_UI'`, () => {
+  it(`should have as title 'Social_App'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Social_UI');
+    expect(app.title).toEqual('Social_App');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Social_UI app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Social_App app is running!');
   });
 });
--- Social_App/src/app/app.component.ts
+++ Social_App/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Social_UI';
+  title = 'Social_App';
 }
